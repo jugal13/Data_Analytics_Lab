2b0e7dc7ca213113be07cf7dada691ab0d885527
Jugal <jugaldeepak@gmail.com>
2019-09-18 12:24:02 +0530

    lr

--- Week 3/Untitled.R
+++
@@ -1,9 +0,0 @@
-data = read.csv("advert.csv")
-LinearRegression <- function() {
-  x = data[,2]
-  y = data[,3]
-  b0 = ((sum(y)*sum(x^2))-(sum(x)*sum(x*y)))/length(x[1])
-}
-
-x = data[,2]
-lenght(x)
